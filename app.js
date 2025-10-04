5dd30023eda2987f0a302d9ced49ba66babe810a
ShravanRamjathan <shravanramjathan@gmail.com>
2025-10-04 15:23:17 +0200

    refactor: :recycle: Fixed missing import in routes
    This means the pantry apis are fully up and running, which can now be connected to the app

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ const app = express();
 // Routes
 const authRoutes = require("./routes/authRoutes");
 const healthRoutes = require("./routes/healthRoutes");
+const pantryRoutes = require("./routes/pantryRoutes");
 const verifyToken = require("./middleware/authMiddleware");
-const createPantry = require("./controllers/pantyController");
+
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/", healthRoutes);
-app.use("/api/pantry", verifyToken, createPantry);
+app.use("/api/pantry", verifyToken, pantryRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
--- routes/pantryRoutes.js
+++ routes/pantryRoutes.js
@@ -1,20 +1,21 @@
 const express = require("express");
-const createPantry = require("../controllers/pantyController");
 const router = express.Router();
+const verifyToken = require("../middleware/authMiddleware");
+const pantryController = require("../controllers/pantyController");
 
 // POST /api/pantry
-router.post("/", pantryController.addItem);
+router.post("/", verifyToken, pantryController.addItem);
 
 // GET /api/pantry
-router.get("/", pantryController.getAllItems);
+router.get("/", verifyToken, pantryController.getAllItems);
 
 // GET /api/pantry/:id
-router.get("/:id", pantryController.getItemById);
+router.get("/:id", verifyToken, pantryController.getItemById);
 
 // PUT /api/pantry/:id
-router.put("/:id", pantryController.updateItem);
+router.put("/:id", verifyToken, pantryController.updateItem);
 
 // DELETE /api/pantry/:id
-router.delete("/:id", pantryController.deleteItem);
+router.delete("/:id", verifyToken, pantryController.deleteItem);
 
 module.exports = router;
